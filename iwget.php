2dbf14839017a1c3bb0c00a4420d0ad14279264a
Zingus J. Rinkle <zingus@gmail.com>
2014-12-19 04:53:34 +0100

    iwget with revised maxLength on WINDOWS and fixed MaxArgs (found previously broken)

--- iwget.php
+++ iwget.php
@@ -1,7 +1,6 @@
-#!/usr/bin/env php
 <?php
 
-ob_start(); include('induce'); ob_end_clean();
+ob_start(); include('induce.php'); ob_end_clean();
 
 /*
 $zero=$argv[0];
@@ -10,40 +9,78 @@ if($ok) $wget=$m[1];
 */
 
 $inducted=array();
-$maxargs=256;
-$optargs=0;
+$maxLength=null;
+$maxArgs=256;
 $args=array_slice($argv,1);
 // wget forgeries
 $forge_user_agent="-U 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'";
 $ignore_robots="-e robots=off";
 
-$cmd="wget -c $forge_user_agent $ignore_robots";
+if(PHP_OS=='WINNT') {
+  // $maxArgs=null; // IDK, is there a cygwin/mingw limitation here?
+  //$maxLength=8191; // shall be ok for XP and further
+  $maxLength=2047; // IDK, Windows 2k an previous
+}
 
-$cmd_args=array();
+$cmd="wget -c $forge_user_agent $ignore_robots";
+$cmdArgCount=5;
+
+$commandArgs=array();
 foreach($args as $arg) {
   if(strstr($arg,'://')) {
     // urls
     $inducted=array_merge($inducted,induce($arg));
   } else {
     // command line options
-    $optargs++;
-
     $cmd.=" '$arg'";
+    $cmdArgCount++;
   }
 }
 
-$scpt="";
+/*
+$script="";
 while($inducted) {
-  $slice=array_splice($inducted,0,$maxargs-$optargs);
-  $cmd_args=array();
+  $slice=array_splice($inducted,0,$maxArgs-$cmdArgCount);
+  $commandArgs=array();
   foreach($slice as $arg) {
-    $cmd_args[]="--referer=$arg"; // forge referer
-    $cmd_args[]="$arg";
+    $commandArgs[]="--referer=$arg"; // forge referer
+    $commandArgs[]="$arg";
   }
-  $cmd_args=implode("' '",$cmd_args);
-  $scpt.="$cmd '$cmd_args' \n";
+  $commandArgs=implode("' '",$commandArgs);
+  $script.="$cmd '$commandArgs' \n";
+}
+*/
+
+$script=array();
+$commandLine="$cmd";
+$argCount=$cmdArgCount;
+while($inducted) {
+  $arg=array_shift($inducted);
+  $addenda=" --referer='$arg' '$arg'"; // add argument and forge referer
+  $addendaArgCount=2;
+
+  $maxLengthReached = is_null($maxLength) ? false : strlen($commandLine) + strlen($addenda) >= $maxLength;
+  $maxArgsReached   = is_null($maxArgs)   ? false : $argCount >= $maxArgs;
+  
+  if($maxLengthReached or $maxArgsReached) {
+    $script[]=$commandLine;
+    $commandLine="$cmd $addenda";
+    $argCount=$cmdArgCount;
+  }
+  
+  $commandLine.=$addenda;
+  $argCount+=$addendaArgCount;
+}
+$script[]=$commandLine;
+
+foreach($script as $commandLine) {
+  echo $commandLine,"\n";
+  system($commandLine);
 }
 
-//$scpt.='wait';
-echo $scpt;
-system("$scpt");
+var_export($script);
+
+$fp=fopen('wgetScript.bat','w');
+foreach($script as $commandLine)
+  fwrite($fp,$commandLine."\r\n");
+fclose($fp);
